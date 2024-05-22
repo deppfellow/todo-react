b4b4012dbec00810c0fe2783d65b72a6511cf1e3
RaidRafif <raid.rafif3@gmail.com>
2024-05-22 16:30:03 +0700

    Add header and the styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,83 @@
+import Header from './components/Header';
+import './App.css';
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Header />
+			<main className="content-container">
+				<ul className="project-list">
+					<li className="project-unit">
+						<span>PROJECT 1</span>
+						<button className="btn btn-add">Add</button>
+						<button className="btn btn-warning btn-edit">
+							Edit
+						</button>
+						<button className="btn btn-danger btn-delete">
+							Delete
+						</button>
+
+						<ul className="todo-list">
+							<li className="todo-unit">
+								<label>
+									<input type="checkbox" />
+									<span>Todo unit 1</span>
+								</label>
+								<button className="btn btn-warning btn-edit">
+									Edit
+								</button>
+								<button className="btn btn-danger btn-delete">
+									Delete
+								</button>
+							</li>
+							{/* Another <li /> for any additional todo unit */}
+						</ul>
+					</li>
+
+					{/* Another <li /> for another projects */}
+					<li className="project-unit">
+						<span>PROJECT 2</span>
+						<button className="btn btn-add">Add</button>
+						<button className="btn btn-warning btn-edit">
+							Edit
+						</button>
+						<button className="btn btn-danger btn-delete">
+							Delete
+						</button>
+
+						<ul className="todo-list">
+							<li className="todo-unit">
+								<label>
+									<input type="checkbox" />
+									<span>Todo unit 1</span>
+								</label>
+								<button className="btn btn-warning btn-edit">
+									Edit
+								</button>
+								<button className="btn btn-danger btn-delete">
+									Delete
+								</button>
+							</li>
+
+							{/* Another <li /> for any additional todo unit */}
+							<li className="todo-unit">
+								<label>
+									<input type="checkbox" />
+									<span>Todo unit 2</span>
+								</label>
+								<button className="btn btn-warning btn-edit">
+									Edit
+								</button>
+								<button className="btn btn-danger btn-delete">
+									Delete
+								</button>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</main>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+	return (
+		<div className="header">
+			<button className="btn btn-add btn-project" role="button">
+				Add Project
+			</button>
+			<a href="#" className="logo-text">
+				TODO.DEV
+			</a>
+		</div>
+	);
+}
+
+export default Header;
